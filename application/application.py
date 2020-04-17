5b483aa097d9439d58b653c9d50045df19998cdf
Julija Kondratjeva <julija17@gmail.com>
2020-04-17 12:02:49 +0300

    update columns type

--- application/application.py
+++ application/application.py
@@ -11,8 +11,8 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     __tablename__ = 'todos'
 
-    id = db.Column(db.INTEGER, primary_key=True)
-    description = db.Column(db.String, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
 
     def __repr__(self):
         return f'<ID: {self.id}, Description: {self.description}>'
