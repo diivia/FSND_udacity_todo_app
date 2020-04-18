f96d109039d3ddda636e4a04c7d3f57f64b2e6a5
Julija Kondratjeva <julija17@gmail.com>
2020-04-18 21:30:26 +0300

    add generated migrations

--- application/application.py
+++ application/application.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://julijakondratjeva@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
-    __tablename__ = 'todos'
+    __tablename__ = ' '
 
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String, nullable=False)
@@ -18,19 +20,31 @@ class Todo(db.Model):
         return f'<ID: {self.id}, Description: {self.description}>'
 
 
-db.create_all()
+#db.create_all()
 
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.get_json()['description']
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    print(todo.description)
-    return jsonify({
-        'description': todo.description
-    })
+    body = {}
+    error = False
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        print(todo.description)
+        body['description']=todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort (400)
+    else:
+        return jsonify(body)
+
 
 
 @app.route('/')
