99373847a494a4f996a691c457fcee726d086c55
Julija Kondratjeva <julija17@gmail.com>
2020-04-18 19:12:47 +0300

    send requests asyncroniusly

--- application/application.py
+++ application/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -12,13 +12,25 @@ class Todo(db.Model):
     __tablename__ = 'todos'
 
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String, nullable=False)
 
     def __repr__(self):
         return f'<ID: {self.id}, Description: {self.description}>'
 
 
 db.create_all()
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    print(todo.description)
+    return jsonify({
+        'description': todo.description
+    })
 
 
 @app.route('/')
