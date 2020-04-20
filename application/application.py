bb81347373120eb90e9511315469d11f95bfb969
Julija Kondratjeva <julija17@gmail.com>
2020-04-20 11:12:47 +0300

    add checkbox

--- application/application.py
+++ application/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, abort
+from flask import Flask, render_template, request, jsonify, abort, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sys
@@ -10,6 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
@@ -18,6 +19,21 @@ class Todo(db.Model):
 
     def __repr__(self):
         return f'<ID: {self.id}, Description: {self.description}>'
+
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -28,7 +44,7 @@ def create_todo():
         todo = Todo(description=description)
         db.session.add(todo)
         db.session.commit()
-        body['description']=todo.description
+        body['description'] = todo.description
     except:
         error = True
         db.session.rollback()
@@ -36,12 +52,11 @@ def create_todo():
     finally:
         db.session.close()
     if error:
-        abort (400)
+        abort(400)
     else:
         return jsonify(body)
 
 
-
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
