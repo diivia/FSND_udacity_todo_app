ac98de36be48dea85df8229ed39c79b9ac37fa93
Julija Kondratjeva <julija17@gmail.com>
2020-04-21 21:10:39 +0300

    add todo lists

--- application/application.py
+++ application/application.py
@@ -11,11 +11,19 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True, collection_class=list, cascade='all')
+
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'<ID: {self.id}, Description: {self.description}>'
